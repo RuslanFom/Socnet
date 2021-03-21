c3387a93b28b5dac7c256a1eaa3ec77eb3309e86
Ruslan Fomin <ruslanfom@gmail.com>
2021-03-21 16:43:48 +0200

    refactor: modify Header

--- 01-first-project/my-app/src/components/Header.jsx
+++ 01-first-project/my-app/src/components/Header.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Header = () => {
     return <header className='header'>
-        <img src="viking.png" alt=""/>
+        <img src="https://cdn1.savepice.ru/uploads/2021/3/21/bea4ad383f82028ae4d2c37cf660e2f2-full.jpg" alt=""/>
     </header>
 }
 export default Header;
