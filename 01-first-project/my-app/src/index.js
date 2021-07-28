b0749f8f783743b4139b5b5bf1c000af486ba876
Ruslan Fomin <ruslanfom@gmail.com>
2021-07-28 19:44:34 +0300

    refactor: add redux-store

--- 01-first-project/my-app/src/index.js
+++ 01-first-project/my-app/src/index.js
@@ -2,7 +2,7 @@ import ReactDOM from 'react-dom';
 import React from 'react';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import store from "./redux/state";
+import store from "./redux/redux-store";
 import App from './App';
 import {BrowserRouter} from "react-router-dom";
 
@@ -20,7 +20,10 @@ let rerenderEntireTree = (state) => {
 
 rerenderEntireTree(store.getState());
 
-store.subscribe(rerenderEntireTree);
+store.subscribe(() => {
+    let state = store.getState();
+    rerenderEntireTree(state);
+});
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
---
+++ 01-first-project/my-app/src/redux/store.js
@@ -0,0 +1,63 @@
+import profileReducer from "./profileReducer";
+import dialogsReducer from "./dialogsReducer";
+import sidebarReducer from "./sidebarReducer";
+
+let store = {
+    _state : {
+        profilePage: {
+            posts: [
+                {id: 1, message: 'Hi, how are you?', likesCount: 12},
+                {id: 2, message: 'It\'s my first post', likesCount: 11},
+                {id: 3, message: 'What did you say, dude?', likesCount: 10},
+                {id: 4, message: 'Who am I?', likesCount: 15}
+            ],
+            newPostText: 'ruslan.com'
+        },
+        dialogsPage: {
+            dialogs: [
+                {id: 1, name: 'Ruslan'},
+                {id: 2, name: 'Olga'},
+                {id: 3, name: 'Viktor'},
+                {id: 4, name: 'Alex'},
+                {id: 5, name: 'Tatiana'},
+                {id: 6, name: 'Oleg'}
+            ],
+            messages: [
+                {id: 1, message: 'Hi'},
+                {id: 2, message: 'Whats uuupp'},
+                {id: 3, message: 'Cmoon man'},
+                {id: 4, message: 'Who let the dogs now'},
+                {id: 5, message: 'Where is your mom'}
+            ],
+
+            newMessageBody: ''
+        },
+        sidebar : {}
+
+    },
+    _callSubscriber()  {
+        console.log('State was changed');
+    },
+
+    getState() {
+        return this._state;
+    },
+    subscribe(observer) {
+        this._callSubscriber = observer;
+    },
+    dispatch(action){ // { type: '(что именно нужно сделать)'} // {type: 'ADD-POST' }
+
+        this._state.profilePage = profileReducer(this._state.profilePage, action);
+        this._state.dialogsPage = dialogsReducer(this._state.dialogsPage, action);
+        this._state.sidebar = sidebarReducer(this._state.sidebar, action);
+
+        this._callSubscriber(this._state);
+    }
+}
+
+window.store = store;
+
+export default store;
+
+
+//store is OOP
