802d5b7b620a6e6d5473f251ee03c456e797a443
ruslanfom <ruslanfom@gmail.com>
2021-08-08 15:16:29 +0300

    install react-redux

--- 01-first-project/my-app/src/components/Header/Header.jsx
+++ 01-first-project/my-app/src/components/Header/Header.jsx
@@ -3,7 +3,7 @@ import s from './Header.module.css';
 
 const Header = () => {
     return <header className={s.header}>
-        <img src="https://cdn1.savepice.ru/uploads/2021/6/30/984f0d5097110bc87414254d13e2c2a9-full.jpg" alt=""/>
+        <img src="https://icdn.lenta.ru/images/2019/12/20/13/20191220133751970/pic_40dd556e7f5266965295eb86cc44430c.jpg" alt=""/>
     </header>
 }
 export default Header;
