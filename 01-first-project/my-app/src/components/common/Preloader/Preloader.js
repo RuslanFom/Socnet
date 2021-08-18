05ef52ff0a1c0e13cd19f2ea9055411c963bf6c4
ruslanfom <ruslanfom@gmail.com>
2021-08-18 22:55:19 +0300

    add Preloader, isFetching indication

---
+++ 01-first-project/my-app/src/components/common/Preloader/Preloader.js
@@ -0,0 +1,8 @@
+import React from "react";
+import Loader from "../../../assets/images/Loader.svg";
+
+let Preloader = (props) => {
+    return <div><img src={Loader}/></div>
+}
+
+export default Preloader;
