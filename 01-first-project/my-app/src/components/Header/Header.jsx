0937a10b4041460e6e2e3c63ce4fe95e1116632b
ruslanfom <ruslanfom@gmail.com>
2021-08-18 17:39:32 +0300

    refactor: usersContainer

--- 01-first-project/my-app/src/components/Header/Header.jsx
+++ 01-first-project/my-app/src/components/Header/Header.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import s from './Header.module.css';
+import viking from '../../assets/images/viking.png'
 
 const Header = () => {
     return <header className={s.header}>
-        <img src="https://icdn.lenta.ru/images/2019/12/20/13/20191220133751970/pic_40dd556e7f5266965295eb86cc44430c.jpg" alt=""/>
+        <img src={viking} alt=""/>
     </header>
 }
 export default Header;
--- 01-first-project/my-app/src/components/Profile/MyPosts/Post/Post.jsx
+++ 01-first-project/my-app/src/components/Profile/MyPosts/Post/Post.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import s from './Post.module.css';
+import usersLogo from '../../../../assets/images/usersLogo.jpg'
 
 const Post = (props) => {
     return (
         <div className={s.item}>
-            <img src="https://telegraf.com.ua/files/2019/04/p_548388141.jpg" alt=""/>
+            <img src={usersLogo} alt=""/>
             {props.message}
             <div>
                 <span>like</span> {props.likesCount}
