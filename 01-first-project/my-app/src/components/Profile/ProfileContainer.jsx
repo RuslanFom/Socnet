ec366d4a4b5c0f7d16f33f64c5be6f83accd9319
Ruslan Fomin <ruslanfom@gmail.com>
2021-08-28 16:06:41 +0300

    add ProfileStatus, editMode, setState

--- 01-first-project/my-app/src/components/Profile/ProfileContainer.jsx
+++ 01-first-project/my-app/src/components/Profile/ProfileContainer.jsx
@@ -31,7 +31,6 @@ let mapStateToProps = (state) => ({
 
 export default compose(
     connect(mapStateToProps,{getUserProfile}),
-    withRouter,
-    withAuthRedirect
+    withRouter
 )(ProfileContainer);
 
--- 01-first-project/my-app/src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ 01-first-project/my-app/src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import s from './ProfileInfo.module.css';
 import Preloader from "../../common/Preloader/Preloader";
+import ProfileStatus from "./ProfileStatus"
 
 const ProfileInfo = (props) => {
     if (!props.profile) {                   /* "!" - это отрицание, если "props.profile отсутствует"*/
@@ -8,14 +9,14 @@ const ProfileInfo = (props) => {
     }
     return (
         <div>
-            <div>
+           {/* <div>
                 <img
                     src="https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/Landscape-Color.jpg"
                     alt=""/>
-            </div>
+            </div>*/}
             <div className={s.descriptionBlock}>
                 <img src={props.profile.photos.large} />
-                ava + description
+                <ProfileStatus status={"У меня полчается!!!"}/>
             </div>
         </div>
     )
